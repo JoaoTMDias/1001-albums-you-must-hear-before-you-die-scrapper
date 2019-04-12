1c5d0f32d9902e0cbcee1ca95a8982b196ddcf77
João Dias <>
2019-04-12 11:47:29 +0100

    Refactored search algorithm and metadata parsing.

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -8,15 +8,58 @@ const parsedResults = [];
 const pageLimit = 26;
 let pageCounter = 0;
 let resultCount = 0;
-const url =
-  `https://www.listchallenges.com/1001-albums-you-must-hear-before-you-die-2016/checklist/${pageCounter + 1}`;
 
+const sourceUrl = 'https://www.listchallenges.com/1001-albums-you-must-hear-before-you-die-all-2018/checklist/'
+const url =
+  `${sourceUrl}${pageCounter + 1}`;
 
 console.log(
   chalk.yellow.bgBlue(
     `\n  Scraping of ${chalk.underline.bold(url)} initiated...\n`
   )
 );
+
+const dividers = [
+  "- ",
+  ", ",
+];
+
+
+const extractMetadata = (title) => {
+  if(title){
+    const titleDividerOption0 = title.split(dividers[0]);
+    const titleDividerOption1 = title.split(dividers[1]);
+
+    let part1 = null;
+    let part2 = null;
+    let part3 = null;
+
+    if (titleDividerOption0.length > 1) {
+      part1 = titleDividerOption0[0].trim();
+      part2 = titleDividerOption0[1].trim();
+    } else if (titleDividerOption1.length > 1) {
+      part1 = titleDividerOption1[0].trim();
+      part2 = titleDividerOption1[1];
+    }
+
+    yearSearch = part2 ? /\d{4}?/g.exec(part2) : '';
+    part3 = yearSearch ? yearSearch[0] : null;
+
+    if(part3){
+      let removeYear = part2.replace(part3, '');
+      let removeParenthesis = removeYear ? removeYear.replace(' ()', '') : removeYear;
+      part2 = removeParenthesis;
+    }
+
+    return {
+      artist: part1,
+      album: part2,
+      year: part3,
+    };
+  }
+
+  return null;
+}
 
 const getWebsiteContent = async url => {
   try {
@@ -30,30 +73,33 @@ const getWebsiteContent = async url => {
         .text()
         .trim();
 
-      const splitTitle = title.split(" - ");
-      const artist = splitTitle ? splitTitle[0] : null;
-      const album = splitTitle ? splitTitle[1] : null;
+      const metadata = extractMetadata(title);
+      const hasResults = metadata ? true : false;
+
+
+      const artist = metadata && hasResults ? metadata.artist :  null;
+      const album = metadata && hasResults ? metadata.album : null;
+      const year = metadata && hasResults ? metadata.year : null;
+
       const src = $(el)
         .find(".item-image-wrapper img")
         .attr("src");
-      const metadata = {
+      const data = {
         id: count,
         title: title,
         artist: artist,
         album: album,
-        year: null,
-        genre: null,
-        recordLabel: null,
+        year: year,
         cover: `https://www.listchallenges.com${src}`,
       };
-      parsedResults.push(metadata);
+      parsedResults.push(data);
     });
 
     // Pagination Elements Link
     pageCounter++;
 
     const nextPageLink =
-      `https://www.listchallenges.com/1001-albums-you-must-hear-before-you-die-2016/checklist/${pageCounter + 1}`;
+      `${sourceUrl}${pageCounter + 1}`;
 
     console.log(chalk.cyan(`  Scraping: ${nextPageLink}`));
 
